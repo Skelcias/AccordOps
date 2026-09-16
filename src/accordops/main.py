51c5d5831c9f418f1c8904bd31d71fa3aea2b22d
Arthur Robert <arthur.robert@airtahitinui.pf>
2026-09-16 13:43:31 -1000

    L0.0: naive CSV comparison with zip

---
+++ src/accordops/main.py
@@ -0,0 +1,26 @@
+import csv
+
+## Chargement des datasets
+
+with open('dataset/policy.csv',newline='',encoding='utf-8') as file :
+    reader = csv.DictReader(file)
+    policy = list(reader)
+
+with open('dataset/expenses.csv',newline='',encoding='utf-8') as file :
+    reader = csv.DictReader(file)
+    expenses = list(reader)
+
+## fonction qui compare 2 item et print CONFORME  ou NON CONFORME + x e de non conformité
+
+def compare_with_policy(policy_value:float,expense_value:float)->str:
+
+    return 'Conforme' if policy_value - expense_value >= 0 else f'Non conforme de {policy_value - expense_value:.2f} $'
+ 
+if __name__ == '__main__':
+    answers:list[str] = []
+    for a , b in zip(policy,expenses):
+        if a["category"] == b["category"]:
+            answers.append(compare_with_policy(float(a["max_amount"]),float(b["amount"])))
+    for elem in answers:
+        print(elem)
+    
